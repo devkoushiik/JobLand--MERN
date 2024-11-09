0944069fab75316af5fc7f5cea2ab83242552494
koushikxahmed <koushikxahmed@gmail.com>
2024-11-09 08:15:49 +0600

    admin page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,7 @@ import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
+import { loader as adminLoader } from "./pages/Admin";
 
 // this will run for all routes so placement is important here.
 export const checkDefaultTheme = () => {
@@ -57,7 +58,7 @@ const router = createBrowserRouter([
           { path: "stats", element: <Stats /> },
           { path: "all-jobs", element: <AllJobs />, loader: allJobsLoader },
           { path: "profile", element: <Profile /> },
-          { path: "admin", element: <Admin /> },
+          { path: "admin", element: <Admin />, loader: adminLoader },
           {
             path: "edit-jobs/:id",
             element: <EditJob />,
