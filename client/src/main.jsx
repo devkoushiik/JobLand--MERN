54683394ed3800b3bb20ad8d0b2a42c8e8add389
koushikxahmed <koushikxahmed@gmail.com>
2024-11-16 19:18:24 +0600

    unnecessary import removed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
-import App from './App.jsx'
-import customFetch from "./utils/customFetch.js";
+import App from "./App.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
